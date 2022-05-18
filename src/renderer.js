93ffca1c27569bf1e78c93b17e81ba8acafe085c
Antoine Groudiev <antoine.groudiev@gmail.com>
2022-05-18 19:54:43 +0200

    Added a resizable PDF pannel (with a placeholder)

--- src/renderer.js
+++ src/renderer.js
@@ -2,11 +2,13 @@ const { ipcRenderer } = require("electron");
 const path = require("path");
 
 window.addEventListener("DOMContentLoaded", () => {
+    /* BUTTONS */
+
     const el = {
         documentName: document.getElementById("documentName"),
         createDocumentBtn: document.getElementById("createDocumentBtn"),
         openDocumentBtn: document.getElementById("openDocumentBtn"),
-        fileTextarea: document.getElementById("fileTextarea"),
+        fileTextarea: document.getElementById("codeTextArea"),
     };
 
     const handleDocumentChange = (filePath, content = "") => {
@@ -36,4 +38,68 @@ window.addEventListener("DOMContentLoaded", () => {
     ipcRenderer.on("document-opened", (_, {filePath, content}) => {
         handleDocumentChange(filePath, content);
     });
+
+
+    /* RESIZABLE AREAS */
+
+    // Query the element
+    const resizer = document.getElementById("codePdfResizer");
+    const leftSide = document.getElementById('codeTextArea');
+    const rightSide = document.getElementById("pdfView");
+
+    // The current position of mouse
+    let x = 0;
+    let y = 0;
+
+    // Width of left side
+    let leftWidth = 0;
+
+    // Handle the mousedown event
+    // that's triggered when user drags the resizer
+    const mouseDownHandler = function (e) {
+        // Get the current mouse position
+        x = e.clientX;
+        y = e.clientY;
+        leftWidth = leftSide.getBoundingClientRect().width;
+
+        // Attach the listeners to `document`
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler);
+    };
+
+    // Attach the handler
+    resizer.addEventListener('mousedown', mouseDownHandler);
+
+    const mouseMoveHandler = function (e) {
+        // How far the mouse has been moved
+        const dx = e.clientX - x;
+        const dy = e.clientY - y;
+
+        const newLeftWidth = ((leftWidth + dx) * 100) / resizer.parentNode.getBoundingClientRect().width;
+        leftSide.style.width = `${newLeftWidth}%`;
+
+        document.body.style.cursor = 'col-resize';
+
+        leftSide.style.userSelect = 'none';
+        leftSide.style.pointerEvents = 'none';
+
+        rightSide.style.userSelect = 'none';
+        rightSide.style.pointerEvents = 'none';
+    };
+
+    const mouseUpHandler = function () {
+        resizer.style.removeProperty('cursor');
+        document.body.style.removeProperty('cursor');
+
+        leftSide.style.removeProperty('user-select');
+        leftSide.style.removeProperty('pointer-events');
+
+        rightSide.style.removeProperty('user-select');
+        rightSide.style.removeProperty('pointer-events');
+
+        // Remove the handlers of `mousemove` and `mouseup`
+        document.removeEventListener('mousemove', mouseMoveHandler);
+        document.removeEventListener('mouseup', mouseUpHandler);
+    };
+
 });
