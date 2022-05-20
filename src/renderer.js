2e05f4e954db401459a6d51b1202fe81257065bd
Antoine Groudiev <antoine.groudiev@gmail.com>
2022-05-20 22:03:40 +0200

    Feature to resize the side panel

--- src/renderer.js
+++ src/renderer.js
@@ -43,63 +43,81 @@ window.addEventListener("DOMContentLoaded", () => {
     /* RESIZABLE AREAS */
 
     // Query the element
-    const codePdfresizer = document.getElementById("codePdfResizer");
-    const codeEditorPanel = document.getElementById('codeEditorPanel');
-    const pdfViewerPanel = document.getElementById("pdfViewerPanel");
+    const resizer = [document.getElementById("codePdfResizer"), document.getElementById("sideDevelopmentResizer")];
+    const leftSide = [document.getElementById("codeEditorPanel"), document.getElementById("sidePanel")];
+    const rightSide = [document.getElementById("pdfViewerPanel"), document.getElementById("devAndFooterContainer")];
 
+    // Store the functions that might need to be removed later
+    let mouseUpFuncs = [];
+    let mouseMoveFuncs = [];
+
+    for (var i = 0 ; i < resizer.length ; i++) {
+        const id = i;
+        mouseMoveFuncs.push((e) => { mouseMoveHandler(e, id); });
+        mouseUpFuncs.push(() => { mouseUpHandler(id); });
+    }
+    
     // The current position of mouse
     let x = 0;
     let y = 0;
 
     // Width of left side
-    let leftWidth = 0;
+    let leftWidth = [0, 0];
 
     // Handle the mousedown event
     // that's triggered when user drags the resizer
-    const mouseDownHandler = function (e) {
+    const mouseDownHandler = function (e, id) {
         // Get the current mouse position
         x = e.clientX;
         y = e.clientY;
-        leftWidth = codeEditorPanel.getBoundingClientRect().width;
+        leftWidth[id] = leftSide[id].getBoundingClientRect().width;
 
         // Attach the listeners to `document`
-        document.addEventListener('mousemove', mouseMoveHandler);
-        document.addEventListener('mouseup', mouseUpHandler);
+        document.addEventListener('mousemove', mouseMoveFuncs[id]);
+        document.addEventListener('mouseup', mouseUpFuncs[id]);
     };
 
-    // Attach the handler
-    codePdfresizer.addEventListener('mousedown', mouseDownHandler);
-
-    const mouseMoveHandler = function (e) {
+    const mouseMoveHandler = function (e, id) {
         // How far the mouse has been moved
         const dx = e.clientX - x;
         const dy = e.clientY - y;
 
-        const newLeftWidth = ((leftWidth + dx) * 100) / codePdfresizer.parentNode.getBoundingClientRect().width;
-        codeEditorPanel.style.width = `${newLeftWidth}%`;
+        const parentWidth = resizer[id].parentNode.getBoundingClientRect().width;
+        const newLeftWidth = ((leftWidth[id] + dx) * 100) / parentWidth;
+        const newRightWidth =  ((parentWidth - leftWidth[id] - dx) * 100) / parentWidth;
+
+        leftSide[id].style.width = `${newLeftWidth}%`;
+        rightSide[id].style.width = `${newRightWidth}%`;
+
+        // TODO: ADAPT RIGHT SIDE ALSO
 
         document.body.style.cursor = 'col-resize';
 
-        codeEditorPanel.style.userSelect = 'none';
-        codeEditorPanel.style.pointerEvents = 'none';
+        leftSide[id].style.userSelect = 'none';
+        leftSide[id].style.pointerEvents = 'none';
 
-        pdfViewerPanel.style.userSelect = 'none';
-        pdfViewerPanel.style.pointerEvents = 'none';
+        rightSide[id].style.userSelect = 'none';
+        rightSide[id].style.pointerEvents = 'none';
     };
 
-    const mouseUpHandler = function () {
-        codePdfresizer.style.removeProperty('cursor');
+    const mouseUpHandler = function (id) {
+        resizer[id].style.removeProperty('cursor');
         document.body.style.removeProperty('cursor');
 
-        codeEditorPanel.style.removeProperty('user-select');
-        codeEditorPanel.style.removeProperty('pointer-events');
+        leftSide[id].style.removeProperty('user-select');
+        leftSide[id].style.removeProperty('pointer-events');
 
-        pdfViewerPanel.style.removeProperty('user-select');
-        pdfViewerPanel.style.removeProperty('pointer-events');
+        rightSide[id].style.removeProperty('user-select');
+        rightSide[id].style.removeProperty('pointer-events');
 
         // Remove the handlers of `mousemove` and `mouseup`
-        document.removeEventListener('mousemove', mouseMoveHandler);
-        document.removeEventListener('mouseup', mouseUpHandler);
+        document.removeEventListener('mousemove', mouseMoveFuncs[id]);
+        document.removeEventListener('mouseup', mouseUpFuncs[id]);
     };
 
+    // Attach the handler
+    for (var i = 0 ; i < resizer.length ; i++) {
+        const id = i;
+        resizer[id].addEventListener('mousedown', (e) => { mouseDownHandler(e, id); });
+    }
 });
