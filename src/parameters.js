2976bb550471242eba8937643f140ab7e66f1838
Red-Rapious <red.rapious@gmail.com>
2022-07-01 12:46:12 +0200

    Changed icons

--- src/parameters.js
+++ src/parameters.js
@@ -1,7 +1,7 @@
 const AUTOSAVE_INTERVAL = 5*60*1000; // 5 minutes
 
 /* DEBUG */
-const STARTUP_INSPECTOR = false;
+const STARTUP_INSPECTOR = true;
 const isDevelopementEnvironement = process.env.NODE_ENV === "development";
 
 module.exports = { AUTOSAVE_INTERVAL, STARTUP_INSPECTOR, isDevelopementEnvironement };
